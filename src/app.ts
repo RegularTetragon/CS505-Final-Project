d4f6511616f3a5a24825f298b409f7ec7f79bbe9
Regular Tetragon <rotflstar@gmail.com>
2020-04-29 20:17:02 -0400

    Finished Schema

--- src/app.ts
+++ src/app.ts
@@ -1,166 +1,41 @@
 import express from "express"
 
 import { PatientStatusCode, positiveCodes, PatientData } from "./types"
-import { connect } from "./subjects";
-import orientdb, { OServer, ORecord, OStatement, OrientDBClient, ODatabaseSession, ODatabaseTransaction, ODatabaseSessionOptions, DropDatabaseOptions, OClassProperty, OClass } from "orientjs"
-import csv from "csv"
-import fs from "fs"
-import { pipeline } from "stream";
+import { rabbitmq } from "./rabbitmq";
+import { OrientDBClient } from "orientjs"
+
 import { create } from "domain";
-    
+import { initializeOrient, orientConfig, schemifyOrient, populateOrient, resetOrient, dblogin } from "./orientsetup";
 
-const orientConfig : orientdb.ServerConfig = {
-    host : "localhost",
-    port: 2424
-}
-
-const dblogin : ODatabaseSessionOptions = {
-    name: 'cs505-final',
-    username: 'root',
-    password: 'rootpwd'
-}
- 
-
-function FileToOrientAsync<T>(db : ODatabaseSession, filename : string, callback : (transaction : ODatabaseSession, current : T)=>any) : Promise<any[]> {
-    return new Promise((res,rej) => {
-        const csvParser = new csv.parse.Parser({
-            delimiter: ",",
-            from_line: 2
-        })
-        let actions : Promise<any>[] = []
-        fs.createReadStream(filename).pipe(csvParser)
-        csvParser.on("error", (err)=>rej(err))
-        csvParser.on("end", ()=>res(Promise.all(actions)))
-        csvParser.on("close", ()=>res(Promise.all(actions)))
-        csvParser.on("data", (data) => {
-            actions.push(callback(db, data))
-        })
-    })
-}
-
-
-async function resetOrient(orient : OrientDBClient) {
-    console.log("Resetting orientDb")
-
-    console.log("Dropping database")
-    await orient.dropDatabase(<DropDatabaseOptions>dblogin)
-    console.log("Creating database")
-    await orient.createDatabase(dblogin)
-    const db = await orient.session(dblogin)
-    console.log("Creating hospital")
-
-    console.log("Creating location");
-    let location = await db.class.create("Location", "V")
-    await location.property.create(
-        [
-            {
-                name: 'zip_code',
-                type: 'Integer',
-                mandatory: true,
-
-            }
-        ]
-    )
-    console.log("Populating OrientDB zipcodes...")
-    let locationRecords : ORecord[] = await FileToOrientAsync<string[]>(db, "./kyzipdetails.csv",
-        (t, [zip,zip_name,city,state,county])=>
-            t.create('VERTEX', 'Location').set({zip_code:zip}).one()
-    )
-    let zipcodeMap = new Map<string,string>();
-    for (let record of locationRecords) {
-        const rid = record["@rid"]
-        const zip_code = (<any>record)["zip_code"]
-        zipcodeMap.set(String(zip_code), "#" + rid!.cluster + ":" + rid!.position)
-    }
-    console.log("Creating zipcodes index...")
-    await db.index.create({
-        type: "UNIQUE",
-        name: "Zipcode",
-        class: "Location",
-        properties: ["zip_code"]
-    })
-
-
-    let hospital = await db.class.create("Hospital", "V")
-    await hospital.property.create(
-        [
-            {
-                name: 'beds',
-                type: 'Integer'
-            },
-            {
-                name: 'id',
-                type: 'String'
-            },
-            {
-                name: 'name',
-                type: 'String'
-            }
-        ]
-    )
-    await hospital.property.create(
-        [
-            {
-                name: 'location',
-                type: 'Link',
-                linkedClass: 'Location'
-            }
-        ]
-    )
-
-    console.log("Populating OrientDB hospital locations...")
-    await FileToOrientAsync<string[]>(db, "./hospitals.csv",
-        (transaction, [ID,NAME,ADDRESS,CITY,STATE,ZIP,TYPE,BEDS,COUNTY,COUNTYFIPS,COUNTRY,LATITUDE,LONGITUDE,NAICS_CODE,WEBSITE,OWNER,TRAUMA,HELIPAD]) =>
-            transaction.create('VERTEX', 'Hospital').set({
-                beds: BEDS,
-                id: ID,
-                name: NAME,
-                location: zipcodeMap.get(ZIP)
-            }).one()
-    );
-    
-    console.log("Creating distance");
-    let distance = await db.class.create("Distance", "E")
-    await distance.property.create(
-        [
-            {
-                name : 'in',
-                type: 'Link',
-                linkedClass: 'Location'
-            },
-            {
-                name : 'out',
-                type: 'Link',
-                linkedClass: 'Location'
-            }
-        ]
-    )
-    await distance.property.create(
-        {
-            name : 'distance',
-            type: 'Double'
-        }
-    )
-
-    
-    console.log("Populating OrientDB distances...")
-    await FileToOrientAsync<string[]>(db, "./kyzipdistance.csv",
-        (t, [zipcode_from, zipcode_to, distance]) => t.create('EDGE', 'Distance')
-            .from(zipcodeMap.get(zipcode_from))
-            .to(zipcodeMap.get(zipcode_to))
-            .set({distance: distance}).exec()
-            
-        )
-    
-    console.log("OrientDB reset sucessful.");
-}
 
 let db_reset_semaphore = 0;
+
+async function pipeRabbitToOrient(orient : OrientDBClient) {
+    let db = await orient.session(dblogin)
+    let rabbit = await rabbitmq((patientData) => {
+        db.update('Patient').set({
+            location: db.select().from('Location').where({'zip_code':patientData.zip_code}),
+            first_name: patientData.first_name,
+            last_name: patientData.last_name,
+            mrn: patientData.mrn,
+            patient_status_code: patientData.patient_status_code
+        }).upsert().where({"mrn": patientData.mrn}).one()
+    })
+    rabbit.on('close', () => db.close())
+    return rabbit
+}
 
 async function main() {
     
     const app = express();
-    const orient = await orientdb.OrientDBClient.connect(orientConfig)
+    const orient = await OrientDBClient.connect(orientConfig)
+    await initializeOrient(orient);
+    await schemifyOrient(orient);
+    await populateOrient(orient);
+
+    pipeRabbitToOrient(orient)
+    
+
     try {
         //await connect(console.log)
     }
@@ -217,7 +92,6 @@ async function main() {
                     res.status(200).send(
                         {reset_status_code: '1'}
                     )
-                    throw "Not implemented"
                 }
                 catch (e) {
                     console.error(e);
